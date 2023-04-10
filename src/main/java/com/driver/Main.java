63410f894fa1633703bdc30567366b40722c40fa
Radhika Bitla <bitlaradhika3@gmail.com>
2023-04-10 22:46:45 +0530

    done till task 6

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
-public class Main {
-  
+public class Main
+{
+    public static void main(String[] args)
+    {
+        RWOnly rwOnly=new RWOnly();
+
+        //when we try to access the name variable of classs RWOnly we get error, since we specified it as private.
+        //and private properties can only be access within the class, and we are trying to access it from another class.
+        System.out.println(rwOnly.name);
+
+        rwOnly.setName("Radhika");
+
+        String value= rwOnly.getName();
+
+        System.out.println(value);
+    }
 }
